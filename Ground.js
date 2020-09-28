6379c37353cd58aa10c3423a3f71bb6f6bb23732
krisha-ops <71079030+krisha-ops@users.noreply.github.com>
2020-09-28 19:17:06 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground{
+    constructor(x,y,width,height){
+        var options ={
+            isStatic : true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.witdh = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill (255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
